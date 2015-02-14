d9dc1d7b209f8f592193a9e495504a0fadb0b474
Piotr Fryga <piotr.fryga@allegro.pl>
2015-02-14 14:45:45 +0100

    mongoose client, connection and carousels endpoints

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require('express');
-var router = express.Router();
+var bodyParser = require('body-parser');
 var mobiusModule = require('./lib/mobiusClient');
+var mongooseModule = require('./lib/mongooseClient');
 var appConfig = require('./config/config.json');
+var router = express.Router();
+
 var mobiusIns = mobiusModule(appConfig.mobius);
+var mongooseIns = mongooseModule(appConfig.mongo);
+var mongoose = mongooseIns.init();
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// models
+
+var Carousel = mongoose.model('Carousel', {
+    key: String,
+    name: String,
+    department: String
+});
 
 // status
 
@@ -23,6 +38,44 @@ router.get('/getOffer/:id', function(req, res) {
         } else {
             res.send(offer);
         }
+    });
+});
+
+// carousels
+
+router.get('/carousels/:department', function(req, res) {
+    Carousel.where({
+        department: req.params.department
+    }).find(function (err, elem) {
+        if (err) return res.send(err);
+        if (elem) {
+            var response = {
+                'collection': req.params.department,
+                'items': elem
+            };
+            res.send(response);
+        }
+    });
+});
+
+router.post('/carousels', function(req, res) {
+    var carousel = new Carousel({
+        key: req.body.key,
+        name: req.body.name,
+        department: req.body.department
+    });
+
+    carousel.save(function () {
+        res.send('inserted!');
+    });
+});
+
+router.delete('/carousels', function(req, res) {
+    Carousel.where().findOneAndRemove({
+        key: req.body.key,
+        department: req.body.department
+    }, function () {
+        res.send('removed!');
     });
 });
 
