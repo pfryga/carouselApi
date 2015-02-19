516b3678ff8ec671100c7e1f5eea87201a56b842
Piotr Fryga <piotr.fryga@allegro.pl>
2015-02-19 21:17:26 +0100

    methods

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ var Offer = mongoose.model('Offer', {
 
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
